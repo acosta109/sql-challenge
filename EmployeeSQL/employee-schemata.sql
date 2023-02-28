080c1b09b03c6959d4530f9d6134872da8db1158
Anthony Costa <acosta1019@gmail.com>
2023-02-28 17:10:22 -0500

    I created the DB in pg4Admin and started working executing the code. I finished the first query.

--- EmployeeSQL/employee-schemata.sql
+++ EmployeeSQL/employee-schemata.sql
@@ -20,7 +20,7 @@ CREATE TABLE "titles" (
 
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
-    "emp_title_id" INTEGER   NOT NULL,
+    "emp_title_id" VARCHAR(30)   NOT NULL,
     "birth_date" VARCHAR(20)   NOT NULL,
     "first_name" VARCHAR(50)   NOT NULL,
     "last_name" VARCHAR(50)   NOT NULL,
@@ -32,8 +32,8 @@ CREATE TABLE "employees" (
 );
 
 CREATE TABLE "dept_manager" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(5)   NOT NULL
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
 );
 
 CREATE TABLE "salaries" (
@@ -49,11 +49,11 @@ CREATE TABLE "dept_emp" (
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
---
+++ EmployeeSQL/query_1_code.sql
@@ -0,0 +1,3 @@
+SELECT employees.emp_no AS Employee_Number, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no;
