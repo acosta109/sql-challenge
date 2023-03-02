7fde0bcd84423a8064b12f5b1513eeddd9c73c00
Anthony Costa <acosta1019@gmail.com>
2023-03-02 11:10:37 -0500

    Removed ind. queries.

--- EmployeeSQL/Individual queries/query-2-code.sql
+++
@@ -1,4 +0,0 @@
---List the first name, last name, and hire date for the employees who were hired in 1986.
-Select employees.first_name, employees.last_name, employees.hire_date
-From employees
-Where employees.hire_date > '1986-1-1'::date And employees.hire_date < '1987-1-1'::date;
--- EmployeeSQL/Individual queries/query-3-code.sql
+++
@@ -1,5 +0,0 @@
---List the manager of each department along with their department number, department name, employee number, last name, and first name.
-Select employees.emp_no, employees.first_name, employees.last_name, dept_manager.dept_no, departments.dept_name
-From employees 
-Join dept_manager ON employees.emp_no = dept_manager.emp_no
-Join departments ON dept_manager.dept_no = departments.dept_no;
